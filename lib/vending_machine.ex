96843a343606779cfe3568999fd814f9adcc9335
Matt Dunson <dunson.142000@gmail.com>
2019-11-10 19:28:22 -0500

    Second commit

--- lib/vending_machine.ex
+++ lib/vending_machine.ex
@@ -1,18 +1,15 @@
 defmodule VendingMachine do
-  @moduledoc """
-  Documentation for VendingMachine.
-  """
+  @server VendingMachine.Server
 
-  @doc """
-  Hello world.
+  def start_link(current_vending_machine) do
+    GenServer.start_link(@server, current_vending_machine, name: @server)
+  end
 
-  ## Examples
+  def insert_coin(coin) do
+    GenServer.cast(@server, {:insert_coin, coin})
+  end
 
-      iex> VendingMachine.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def select_product(product) do
+    GenServer.cast(@server, {:select_product, product})
   end
 end
--- test/vending_machine_test.exs
+++ test/vending_machine_test.exs
@@ -1,8 +1,4 @@
 defmodule VendingMachineTest do
   use ExUnit.Case
   doctest VendingMachine
-
-  test "greets the world" do
-    assert VendingMachine.hello() == :world
-  end
 end
